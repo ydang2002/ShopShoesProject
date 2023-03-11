fba232d715017e506603319abf4a4ed53675926f
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-03-11 11:50:05 +0700

    Thiết kế giao diện quản lý Product, Brand, Category

---
+++ app/src/main/java/com/nhuy/shopshoesproject/view/Activity/Admin/Brand/UpdateBrandActivity.java
@@ -0,0 +1,16 @@
+package com.nhuy.shopshoesproject.view.Activity.Admin.Brand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhuy.shopshoesproject.R;
+
+public class UpdateBrandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_brand);
+    }
+}
---
+++ app/src/main/java/com/nhuy/shopshoesproject/view/Activity/Admin/Category/NewCategoryActivity.java
@@ -0,0 +1,15 @@
+package com.nhuy.shopshoesproject.view.Activity.Admin.Category;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nhuy.shopshoesproject.R;
+
+public class NewCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_category);
+    }
+}
